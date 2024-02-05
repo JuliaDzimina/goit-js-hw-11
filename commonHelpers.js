c71ba4a254086c5ce909700906c9a6756ecf96f4
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2024-02-05 22:10:51 +0000

    Deploying to gh-pages from @ JuliaDzimina/goit-js-hw-11@2c3f790ed4cbcb5fcdafe965f00711ac5f7f80f0 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,10 +1,10 @@
-import"./assets/vendor-491d46cf.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const c={searchForm:document.querySelector(".search-form"),galleryList:document.querySelector(".gallery"),loader:document.querySelector(".loader")};function a(r){const o="42192199-b3b6ebcf3d1600f471f1bd878",n="https://pixabay.com/api/",i=new URLSearchParams({key:o,q:r,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=`${n}?${i}`;return fetch(e).then(t=>{if(t.ok)return t.json();throw new Error(`${t.status}`)})}c.searchForm.addEventListener("submit",l);function l(r){r.preventDefault();const o=r.target.elements.query.value;a(o).then(n=>{d(n.hits)}),r.target.reset()}function u({webformatURL:r,largeImageURL:o,tags:n,likes:i,views:e,comments:t,downloads:s}){return`<li>
-    <img src="" alt="${n}">
-    <figcaption>
+import{i as l,S as c}from"./assets/vendor-46aac873.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))n(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&n(s)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();const a={searchForm:document.querySelector(".search-form"),galleryList:document.querySelector(".gallery"),loader:document.querySelector(".loader")};function u(t){const o="42192199-b3b6ebcf3d1600f471f1bd878",i="https://pixabay.com/api/",n=new URLSearchParams({key:o,q:t,image_type:"photo",orientation:"horizontal",safesearch:!0}),e=`${i}?${n}`;return fetch(e).then(r=>{if(r.ok)return r.json();throw new Error(`${r.status}`)})}a.searchForm.addEventListener("submit",f);function f(t){t.preventDefault();const o=t.target.elements.query.value;u(o).then(i=>{i.hits.length===0&&l.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",backgroundColor:"red",messageColor:"white"}),h(i.hits),new c(".gallery a",{captionDelay:250,captionsData:"alt"})}).catch(i=>{console.log(`Error fetching images: ${i}`)}).finally(a.searchForm.reset()),t.target.reset()}function m({webformatURL:t,largeImageURL:o,tags:i,likes:n,views:e,comments:r,downloads:s}){return`<li class="gallery-list">
+    <a class="gallery-link" href="${o}"><img class="img-gallery" src="${t}" alt="${i}"></a>
+    <figcaption class="info-img">
       
       <div>
         <h3>Views</h3>
-        <p>${i}</p>
+        <p>${n}</p>
       </div>
 
       <div>
@@ -14,7 +14,7 @@ import"./assets/vendor-491d46cf.js";(function(){const o=document.createElement("
 
       <div>
         <h3>Comments</h3>
-        <p>${t}</p>
+        <p>${r}</p>
       </div>
 
       <div>
@@ -23,5 +23,5 @@ import"./assets/vendor-491d46cf.js";(function(){const o=document.createElement("
       </div>
 
     </figcaption>
-  </li>`}function f(r){return r.map(u).join("")}function d(r){const o=f(r);c.galleryList.innerHTML=o}
+  </li>`}function d(t){return t.map(m).join("")}function h(t){const o=d(t);a.galleryList.innerHTML=o}
 //# sourceMappingURL=commonHelpers.js.map
